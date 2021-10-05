a12f5503b7354bfca1b1569bfbfe37b9aab8afe5
yakovsmonarch <medvejonok07@gmail.com>
2021-10-05 15:23:53 +0800

    Created a gun sight.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [SerializeField] private float _speed = 3;
+    [SerializeField] private Vector2 _verticalAngle;
+    [SerializeField] private Vector2 _horizontalAngle;
 
     private Vector2 _rotation = Vector2.zero;
+    private const string MouseX = "Mouse X";
+    private const string MouseY = "Mouse Y";
+
 
     private void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            _rotation.y += Input.GetAxis("Mouse X");
-            _rotation.x += -Input.GetAxis("Mouse Y");
-            _rotation.x = Mathf.Clamp(_rotation.x, -10f, 5f);
-            _rotation.y = Mathf.Clamp(_rotation.y, -10, 10f);
+            _rotation.y += Input.GetAxis(MouseX);
+            _rotation.x += -Input.GetAxis(MouseY);
+            _rotation.x = Mathf.Clamp(_rotation.x, _horizontalAngle.x, _horizontalAngle.y);
+            _rotation.y = Mathf.Clamp(_rotation.y, _verticalAngle.x, _verticalAngle.y);
             transform.eulerAngles = new Vector2(_rotation.x, _rotation.y) * _speed;
         }
     }
